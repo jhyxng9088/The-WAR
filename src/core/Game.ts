fc80f30b83c4934523ff8c69b59097e9321a81f3
jhyxng9088 <j.hyxng9088@gmail.com>
2026-09-11 19:58:23 +0900

    feat: retune atmosphere for stronger relief

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -21,10 +21,10 @@ export class Game {
     });
     this.renderer.outputColorSpace = THREE.SRGBColorSpace;
     this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    this.renderer.toneMappingExposure = 1.02;
+    this.renderer.toneMappingExposure = 0.96;
 
-    this.scene.background = new THREE.Color(0x718f97);
-    this.scene.fog = new THREE.Fog(0x718f97, 110, 225);
+    this.scene.background = new THREE.Color(0x6d8991);
+    this.scene.fog = new THREE.Fog(0x6d8991, 125, 255);
 
     this.camera = new WorldCamera();
     this.input = new WorldInput(canvas, this.camera);
